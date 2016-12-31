f4134c502bd3e4cad45d09de4227adb05c3414f5
netmario <mario-doebler@gmx.net>
2016-12-31 14:10:15 +0800

    Added mask for computing hamming distance

--- hamming_distance.m
+++ hamming_distance.m
@@ -1,4 +1,4 @@
-function [hamming_distance]  = hamming_distance(iris_code1,iris_code2)
-    hamming_distance = sum(sum(abs(iris_code1-iris_code2)))/(size(iris_code1,1)*size(iris_code1,2));
+function [hamming_distance]  = hamming_distance(iris_code1,iris_code2,mask_size)
+    hamming_distance = sum(sum(abs(iris_code1-iris_code2)))/mask_size;
 end
 
--- smallest_distance.m
+++ smallest_distance.m
@@ -1,7 +1,11 @@
-function [smallest_distance] = smallest_distance(iris_code1,iris_code2)
+function [smallest_distance] = smallest_distance(iris_code1,iris_code2,mask1,mask2)
     smallest_distance = Inf;
+    mask = mask1.*mask2;
+    mask_size = sum(sum(mask));
+    iris_code1 = iris_code1.*mask;
+    iris_code2 = iris_code2.*mask;
     for perm = 1:size(iris_code1,2)
-        current_distance = hamming_distance(iris_code1,iris_code2);
+        current_distance = hamming_distance(iris_code1,iris_code2,mask_size);
         if current_distance < smallest_distance
             smallest_distance = current_distance;
         end
